3a27c225306641610ee4c5e6dc4b956999113411
Samuel Beard <sbeard@brightsource.co.uk>
2019-09-09 10:34:46 +0100

    export fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Countdown from './Countdown'
 import { Container, Row, Col } from './Grid'
 import { Card, CardBody, CardHeader, CardFooter } from './Card'
+import Section from './Section'
+import Button from './Button'
 
-export { Countdown, Container, Row, Col, Card, CardBody, CardFooter, CardHeader }
+export { Countdown, Container, Row, Col, Card, CardBody, CardFooter, CardHeader, Section, Button }
