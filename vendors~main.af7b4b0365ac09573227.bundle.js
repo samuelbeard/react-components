6007b58c34c2a600207261f90f1259f4355755a0
Samuel Beard <sbeard@brightsource.co.uk>
2019-09-09 13:15:42 +0100

    Updates

--- vendors~main.af7b4b0365ac09573227.bundle.js
+++ vendors~main.af7b4b0365ac09573227.bundle.js
@@ -113,4 +113,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",function(){return isPlainObject})}]]);
-//# sourceMappingURL=vendors~main.635dc85fbb06bee648e0.bundle.js.map
+//# sourceMappingURL=vendors~main.af7b4b0365ac09573227.bundle.js.map
