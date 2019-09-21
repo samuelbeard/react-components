091c09e28cb4256b93a484602e56c7cfb8b9781c
Samuel Beard <sbeard@brightsource.co.uk>
2019-09-21 11:33:42 +0100

    add background to Row

--- src/Grid/Row.js
+++ src/Grid/Row.js
@@ -2,13 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import cn from 'classnames'
 
-export const Row = ({ className, children, ...props }) => (
-    <div className={cn(`sb-row`, className)} {...props}>
+export const Row = ({ background, className, children, ...props }) => (
+    <div className={cn(`sb-row`, `bg-${background}`, className)} {...props}>
         {children}
     </div>
 )
 
 Row.propTypes = {
+    background: PropTypes.string,
     /** Pass className into the div. */
     className: PropTypes.string,
     children: PropTypes.node.isRequired,
