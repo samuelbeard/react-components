58d35a8364e476ecb6e45729ede784f340803dcc
Samuel Beard <sbeard@brightsource.co.uk>
2019-09-21 11:51:14 +0100

    remove bg on Row

--- src/Grid/Row.js
+++ src/Grid/Row.js
@@ -2,14 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import cn from 'classnames'
 
-export const Row = ({ background, className, children, ...props }) => (
-    <div className={cn(`sb-row`, `bg-${background}`, className)} {...props}>
+export const Row = ({ className, children, ...props }) => (
+    <div className={cn(`sb-row`, className)} {...props}>
         {children}
     </div>
 )
 
 Row.propTypes = {
-    background: PropTypes.string,
     /** Pass className into the div. */
     className: PropTypes.string,
     children: PropTypes.node.isRequired,
